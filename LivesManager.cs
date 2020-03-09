385b4e52f416ab5987413cd20f075534fbf70ab2
BobbyTheDude <61518523+BobbyTheDude@users.noreply.github.com>
2020-03-09 14:41:51 -0700

    Add files via upload

---
+++ LivesManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LivesManager : MonoBehaviour
+{
+    //public GameObject playerInst;
+    public int Lives = 3;
+
+    public void UpdateLives()
+    {
+        GetComponent<Text>().text = "Lives: " + Lives;
+    }
+    //void Start()
+    //{
+       
+    //}
+
+    //// Update is called once per frame
+    void Update()
+    {
+        //if (Dead == true)
+        //{
+        //    GameObject.Instantiate(playerInst);
+        //    Dead = false;
+        //    Lives -= 1;
+        //}
+        //UpdateLives();
+    }
+
+
+}
