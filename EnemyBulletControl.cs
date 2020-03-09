a3327b34403dcfc225648565d192a440b7080785
BobbyTheDude <61518523+BobbyTheDude@users.noreply.github.com>
2020-03-09 14:42:22 -0700

    Add files via upload

---
+++ EnemyBulletControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyBulletControl : MonoBehaviour
+{
+    public float Speed;
+    private Rigidbody2D rb;
+    private Text LivesText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0, Speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Camera.main.WorldToViewportPoint(transform.position).y < 0)
+            Destroy(this.gameObject);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            GameObject.Destroy(this.gameObject);
+            GameObject.Destroy(collision.gameObject);
+            //GetComponent<LivesManager>().Dead = true;
+            LivesText.GetComponent<LivesManager>().Lives -= 1;
+            LivesText.GetComponent<LivesManager>().UpdateLives();
+        }
+    }
+}
+
+
